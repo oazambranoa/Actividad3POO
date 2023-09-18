9c5f125d421a807ed8b56ed7661acd8f370e2d9b
Omar Zambrano <84608091+oazambranoa@users.noreply.github.com>
2023-09-18 11:07:01 -0500

    Primera Parte del trabajo

---
+++ exc2Act3/src/main/java/com/mycompany/exc2act3/Exc2Act3.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exc2act3;
+
+/**
+ *
+ * @author omarz
+ */
+
+/*Enunciado
+19. Dado el valor del lado en un triángulo equilátero, haga un algoritmo que obtenga el
+perímetro, el valor de la altura y el área del triángulo. 
+*/
+public class Exc2Act3 {
+
+    public static void main(String[] args) {
+        
+        e2A3 form = new e2A3();
+        
+        form.setVisible(true);
+    }
+}
---
+++ exc3Act3/src/main/java/com/mycompany/exc3act3/Exc3Act3.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exc3act3;
+
+/**
+ *
+ * @author omarz
+ */
+/*
+Enunciado
+
+ Ejercicio resuelto Nº 7
+Hacer un algoritmo que, dados dos valores numéricos A y B, escriba un mensaje diciendo
+si A es mayor, menor o igual a B.
+Análisis
+Datos de entrada
+• Los valores de A y de B.
+Datos de salida
+• Un mensaje de acuerdo al valor de A y de B.
+Proceso
+Se compara A con B, sí:
+A > B. Entonces se escribe el mensaje «A es mayor que B», si la condición es falsa se
+pregunta sí: 
+A = B. Si ésta es verdadera
+
+*/
+
+public class Exc3Act3 {
+
+    public static void main(String[] args) {
+        
+        e3A3 form = new e3A3();
+        form.setVisible(true);
+        
+    }
+}
