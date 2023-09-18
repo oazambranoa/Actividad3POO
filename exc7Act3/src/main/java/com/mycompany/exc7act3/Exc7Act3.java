66bcd43f51b69a0b95ac89e5426eb127ecd46647
Omar Zambrano <84608091+oazambranoa@users.noreply.github.com>
2023-09-18 11:08:55 -0500

    Ultima parte del trabajo

---
+++ exc7Act3/src/main/java/com/mycompany/exc7act3/Exc7Act3.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.exc7act3;
+
+/**
+ *
+ * @author omarz
+ */
+
+/*
+Se requiere un programa que modele varias figuras geométricas: el círculo,
+el rectángulo, el cuadrado y el triángulo rectángulo.
+u El círculo tiene como atributo su radio en centímetros.
+u El rectángulo, su base y altura en centímetros.
+u El cuadrado, la longitud de sus lados en centímetros.
+u El triángulo, su base y altura en centímetros.
+Se requieren métodos para determinar el área y el perímetro de cada
+figura geométrica. Además, para el triángulo rectángulo se requiere:
+u Un método que calcule la hipotenusa del rectángulo.
+u Un método para determinar qué tipo de triángulo es:
+Equilátero: todos sus lados son iguales.
+Isósceles: tiene dos lados iguales.
+Escaleno: todos sus lados son diferentes.
+Se debe desarrollar una clase de prueba con un método main para
+crear las cuatro figuras y probar los métodos respectivos.
+*/
+public class Exc7Act3 {
+
+    public static void main(String[] args) {
+        mainForm mainForm = new mainForm();
+        mainForm.setVisible(true);
+        
+    }
+}
